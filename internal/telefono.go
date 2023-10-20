bc099fafaf630e4e26a698fc495e017c70598a5c
migueruiz <mruiz812@correo.ugr.es>
2023-10-20 10:24:09 +0200

    Cambio nombre carpeta

---
+++ internal/telefono.go
@@ -0,0 +1,17 @@
+package apellidos
+
+// Defininción de la estructura Telefono
+type Telefono struct {
+	telefono string
+}
+
+// Definición de los métodos de la estructura Telefono
+// Función que permite asignar un telefono a la estructura Telefono
+func (t *Telefono) SetTelefono(telefono string) {
+	t.telefono = telefono
+}
+
+// Función que permite obtener el telefono de la estructura Telefono
+func (t *Telefono) GetTelefono() string {
+	return t.telefono
+}
