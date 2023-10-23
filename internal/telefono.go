4ea645b3b27e50af3ace08eb1ef9a6e0d860079d
migueruiz <mruiz812@correo.ugr.es>
2023-10-23 19:50:26 +0200

    Modificacion comentarios telefono.go
    He quitado todos los comentarios que tiene el código. Closes #12

--- internal/telefono.go
+++ internal/telefono.go
@@ -1,22 +1,17 @@
 package apellidos
 
-// Defininción de la estructura Telefono
 type Telefono struct {
 	telefono string
 }
 
-// Definición de los métodos de la estructura Telefono
-// Función que permite asignar un telefono a la estructura Telefono
 func (t *Telefono) SetTelefono(telefono string) {
 	t.telefono = telefono
 }
 
-// Función que permite obtener el telefono de la estructura Telefono
 func (t *Telefono) Telefono() string {
 	return t.telefono
 }
 
-// Función que permite crear una nueva estructura Telefono
 func NewTelefono(NuevoTelefono string) *Telefono {
 	return &Telefono{
 		telefono: NuevoTelefono,
