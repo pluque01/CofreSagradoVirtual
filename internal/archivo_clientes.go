de201a25d36c73a828ef47d94dde6fbb80ad9691
Pablo Luque Salguero <pablols114@gmail.com>
2023-11-13 20:15:29 +0100

    Según las buenas prácticas no es necesario separar

--- internal/archivo_clientes.go
+++ internal/archivo_clientes.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"regexp"
+)
+
 type ClientFile struct {
 	fileContent [][]string
+	fileTypes   ClientTypes
+}
+
+type ClientTypes struct {
+	types map[string]regexp.Regexp
 }
