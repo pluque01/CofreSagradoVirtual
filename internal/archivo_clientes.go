d71038e8e09be7553a13ddd19622c3f40937a428
Pablo Luque Salguero <pablols114@gmail.com>
2023-11-16 10:20:12 +0100

    Extracción de datos a partir de un archivo, #23

--- internal/archivo_clientes.go
+++ internal/archivo_clientes.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
+	"log"
+	"os"
 	"regexp"
 )
 
 type ClientFile struct {
-	fileContent [][]string
 	fileTypes   ClientTypes
+	fileContent [][]string
 }
 
 type ClientTypes struct {
 	types map[string]regexp.Regexp
 }
 func readFile(filePath string, separator rune) ([][]string, error) {
+	if separator == '\r' || separator == '\n' || separator == '\uFFFD' {
+		return nil, errors.New("invalid separator")
+	}
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal("Error while reading file", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = separator
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Error reading records", err)
+	}
+	return records, nil
 }
+
