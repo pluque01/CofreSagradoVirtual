07b641e3793669c93b5de78dd24349feccd95e01
Pablo Luque Salguero <pablols114@gmail.com>
2023-11-23 17:40:29 +0100

    Implementada sugerencia de cadenas similares, #27
    Se basa en la medición de la similitud de cadenas mediante el algoritmo de Jaro-Winkler para recomendar cadenas parecidas.

--- internal/archivo_clientes.go
+++ internal/archivo_clientes.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 )
 
 type ClientFile struct {
@@ -94,6 +95,136 @@ func (c *ClientFile) ValidateFileContent() *[][]string {
 		results = append(results, columnResults)
 	}
 	return &results
+}
+
+// JaroWinkler function calculates the Jaro-Winkler similarity between two strings
+func JaroWinkler(s1, s2 string) (float64, error) {
+	if len(s1) == 0 || len(s2) == 0 {
+		return 0, errors.New("input strings must not be empty")
+	}
+	// Create two match arrays
+	s1Matches := make([]bool, len(s1))
+	s2Matches := make([]bool, len(s2))
+
+	// Initialize variables
+	matches := 0
+	transpositions := 0
+	halfTranspositions := 0
+
+	// Define the match distance
+	matchDistance := (max(len(s1), len(s2)) / 2) - 1
+
+	// Loop over the first string
+	for i := 0; i < len(s1); i++ {
+		// Define the start and end index for comparison
+		start := max(0, i-matchDistance)
+		end := min(i+matchDistance+1, len(s2))
+
+		// Loop over the second string within the defined range
+		for j := start; j < end; j++ {
+			// If the character in the second string is already matched, continue
+			if s2Matches[j] {
+				continue
+			}
+			// If the characters do not match, continue
+			if s1[i] != s2[j] {
+				continue
+			}
+			// If the characters match, mark them as matched and increment the match count
+			s1Matches[i] = true
+			s2Matches[j] = true
+			matches++
+			break
+		}
+	}
+	// If there are no matches, return 0
+	if matches == 0 {
+		return 0, nil
+	}
+	// Calculate transpositions
+	k := 0
+	for i := 0; i < len(s1); i++ {
+		if !s1Matches[i] {
+			continue
+		}
+		for !s2Matches[k] {
+			k++
+		}
+		if s1[i] != s2[k] {
+			halfTranspositions++
+		}
+		k++
+	}
+	// Calculate the number of transpositions
+	transpositions = halfTranspositions / 2
+	// Calculate the Jaro similarity
+	jaro := ((float64(matches) / float64(len(s1))) +
+		(float64(matches) / float64(len(s2))) +
+		((float64(matches) - float64(transpositions)) / float64(matches))) / 3.0
+	// Calculate the prefix length
+	prefix := 0
+	length := min(len(s1), len(s2))
+	for i := 0; i < length; i++ {
+		if s1[i] != s2[i] {
+			break
+		}
+		prefix++
+	}
+	cl := 0.1
+	if prefix > 4 {
+		prefix = 4
+	}
+	// Return the Jaro-Winkler similarity
+	return jaro + (float64(prefix) * cl * (1 - jaro)), nil
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func RecommendStrings(name string, names *[]string) ([]string, error) {
+	const MaxRecommendations uint8 = 10
+	const MatchThreshold float64 = 0.8
+	matches := []struct {
+		name  string
+		value float64
+	}{}
+	for _, n := range *names {
+		score, err := JaroWinkler(name, n)
+		if err != nil {
+			continue
+		}
+		if score > MatchThreshold {
+			matches = append(matches, struct {
+				name  string
+				value float64
+			}{n, score})
+		}
+	}
+
+	if len(matches) == 0 {
+		return []string{}, errors.New("no matches found")
+	}
+	sort.Slice(matches, func(i, j int) bool {
+		return matches[i].value > matches[j].value
+	})
+	matchesSplit := matches[:min(int(MaxRecommendations), len(matches))]
+	recommendations := []string{}
+
+	for _, recommendation := range matchesSplit {
+		recommendations = append(recommendations, recommendation.name)
+	}
+	return recommendations, nil
 }
 
 func getMatchingRuneIndex(a string, r rune) (int, error) {
