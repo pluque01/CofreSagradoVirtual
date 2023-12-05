6b6fac0e0171646e1129c3ecbd4d6ee7a609dbdd
Pablo Luque Salguero <pablols114@gmail.com>
2023-12-05 17:31:12 +0100

    Uso la salida estándar si no existe config, #40

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -15,18 +15,23 @@ var Default struct {
 }
 
 func init() {
-	var out *os.File
+	var f *os.File
+	var err error
+	out := os.Stdout
 	folder := config.DefaultConfig.LogFolder
-	f, err := os.OpenFile(
-		folder+"validatecsv.log",
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0664,
-	)
-	if err != nil {
-		log.Printf("Error opening log file: %v, using stdout", err)
-		out = os.Stdout
+	if folder != "" {
+		f, err = os.OpenFile(
+			folder+"validatecsv.log",
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+			0664,
+		)
+		if err != nil {
+			log.Printf("Error opening log file: %v, using stdout", err)
+		} else {
+			out = f
+		}
 	} else {
-		out = f
+		log.Printf("No log folder provided, using stdout")
 	}
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
