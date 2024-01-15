3a621a21568ebf42f6926e23797e73c7ea8b9146
Pablo Luque Salguero <pablols114@gmail.com>
2024-01-15 23:09:59 +0100

    La aplicación no necesita un main para testear #46

--- cmd/validcsv/validcsv.go
+++ cmd/validcsv/validcsv.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/pluque01/CofreSagradoVirtual/internal/api"
 	log "github.com/pluque01/CofreSagradoVirtual/internal/logger"
 )
 
 func main() {
-	http.ListenAndServe(":8080", api.GetServeMux())
 	log.Close()
 }
